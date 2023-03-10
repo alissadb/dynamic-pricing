b229230a60c4120adb2e8a7e5fd0c2f9b05175bb
alissadb <alissa_bruijn@hotmail.com>
2023-03-10 11:38:03 +0100

    Add random model

--- modeling/models.py
+++ modeling/models.py
@@ -1,25 +1,54 @@
-"""Models."""
-import pandas as pd
-from loguru import logger
-from sklearn.dummy import DummyRegressor
-from sklearn.model_selection import train_test_split
+"""Model."""
+import functools
+from typing import Final
 
-from modeling.data import train_test_split
+import numpy as np
+from sklearn.base import BaseEstimator, RegressorMixin
+
+PRODUCTS: Final = {
+    "apples-red": (3, 1),  # mean and standard deviation
+    "apples-green": (2.5, 0.5),
+    "bananas": (2, 0.5),
+    "bananas-organic": (2.5, 0.5),
+    "broccoli": (4, 1),
+    "rice": (2, 1),
+    "wine": (6.5, 3),
+    "cheese": (4, 2),
+    "beef": (10, 2),
+    "avocado": (1.5, 0.25),
+}
 
 
-def train_model(data: pd.DataFrame) -> DummyRegressor:
-    """Train a dummy model with the provided data.
+def vectorized(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        vectorized_func = np.vectorize(func)
+        return vectorized_func(*args, **kwargs)
 
-    Args:
-        data (pd.DataFrame): Data to use for training the model.
+    return wrapper
 
-    Returns:
-        DummyRegressor: DummyRegressor model using the median
-    """
-    logger.info("Train model...")
-    X_train, X_test, y_train, y_test = train_test_split(data)
 
-    linear_dummy_median = DummyRegressor(strategy="median")
-    linear_dummy_median.fit(X_train, y_train)
+class SimpleRegression(BaseEstimator, RegressorMixin):
+    def __init__(self):
+        ...
 
-    return linear_dummy_median
+    def fit(self, X, y):
+        """Fit the function."""
+        return self
+
+    @vectorized
+    def get_price(self, product: str) -> float:
+        """Get the price based on the product.
+
+        Args:
+            product (str): Product (for example apples-red, apples-green)
+
+        Returns:
+            float: Price in euro's
+        """
+        mean, standard_deviation = PRODUCTS[product]
+        return np.random.normal(mean, standard_deviation)
+
+    def predict(self, X) -> np.array(float):
+        """Predict the product price based only on the products"""
+        return self.get_price(X)
